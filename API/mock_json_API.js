779dcb1cd83995dbaf9774059c8b3e76588019fa
DJ Hartman <danieljayhartman@gmail.com>
2014-11-23 16:04:39 -0500

    I gate up on trying to get my node script to work, wrote a quick thing in ruby. Commiting the dummy API responses now.

--- API/mock_json_API.js
+++ API/mock_json_API.js
@@ -1,36 +1,39 @@
 // node script for generating .json files
-// node mock_json_API.js
+// I was struggling too much with node and ended up doing this in ruby...
+// Going to look at it again later, but would appeciate help
 
 var fs = require("fs"),
     xml2js = require("xml2js"),
     http = require("http"),
-    _ = require("underscore-node");
+    _ = require("underscore-node"),
+    moment = require("moment")
 
-convertData('/alex.xml', 'API/result.json');
+alexData = fs.readFileSync(__dirname + '/alex.xml');
 
-function convertData(xmlPath, jsonResultPath) {
-  var parser = new xml2js.Parser();
-  var data = {};
+writeJSON(parseXML(alexData), '/result.json');
 
-  // Read the XML File
-  fs.readFile(__dirname + '/alex.xml', function(err, data) {
-      parser.parseString(data, function (err, result) {
-        if (err) throw err;
-
-        console.log("XML converted into JSON...\n");
-
-        // Get the GPS data object
-        data = result['kml']['Document'][0]['Placemark'][0]['gx:Track'][0];
-
-        result = _.map(_.zip(data['when'], data["gx:coord"]), function(point, key) {
-          return {
-            "date": point[0],
-            'location': point[1].split(" "),
-          };
-        });
-
-        fs.writeFile(jsonResultPath, JSON.stringify(result));
-      });
-  });
+function writeJSON(data, filename) {
+  fs.writeFileSync(__dirname + filename, JSON.stringify(data));
 }
+
+function parseXML(data) {
+  var parser = new xml2js.Parser();
+
+  parser.parseString(data, function (err, result) {
+    if (err) throw err;
+
+    // Get the GPS data object
+    data = result['kml']['Document'][0]['Placemark'][0]['gx:Track'][0];
+
+    result = _.map(_.zip(data['when'], data["gx:coord"]), function(point, key) {
+      return {
+        'date': point[0],
+        'location': point[1].split(" ")
+      };
+    });
+
+    return result
+  });
+};
+
 
