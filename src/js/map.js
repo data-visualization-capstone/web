338c8973ed50249db5486cb0d2ab1962d7e6989c
Alex Johnson <alex@tablelist.com>
2015-02-20 01:32:15 -0500

    Generate a random color for new tweet layers

--- src/js/map.js
+++ src/js/map.js
@@ -76,3 +76,31 @@ function verifyKeys(layer){
 
     return layer;
 }
+
+/******************************
+         Coloring
+ ******************************/
+
+// Maps the input number to the output
+// color. Input between 0 and 100 maps
+// to the range of red -> green
+function getColor(i){
+
+  if (i < 0){
+    i = 0;
+  } else if (i > 1){    
+    i = 1;
+  }
+
+  var r = Math.floor(255 * i);
+  var g = Math.floor(255 - 255 * i);
+  var b = 0;
+
+  return componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+// http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
