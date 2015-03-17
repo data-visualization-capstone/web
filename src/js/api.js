f8c19e518e4a12c729e27164736010817e46c154
Alex Johnson <alex@tablelist.com>
2015-03-17 15:57:44 -0400

    Use jquery's $.get as opposed to custom GET function

--- src/js/api.js
+++ src/js/api.js
@@ -7,20 +7,22 @@ DV.api.twitter = {};
 
 // GET /twitter/search/:string
 DV.api.twitter.search = function(string, success, error){
-  DV.api.get("twitter/search/" + string, function(resp){
-    success(resp);
-  }, function(resp){
-    console.error("Error fetching tweets: "  + resp)
-  });
+  $.get(DV.url + "twitter/search/" + string, function(resp){
+      success(resp)
+    })
+    .fail(function() {
+      console.error("Error fetching tweets: "  + resp)
+    })
 }
 
 // GET /twitter/stream/:string
 DV.api.twitter.stream = function(string, success, error){
-  DV.api.get("twitter/stream/" + string, function(resp){
-    success(resp);
-  }, function(resp){
-    console.error("Error fetching tweets: "  + resp)
-  });
+  $.get(DV.url + "twitter/stream/" + string, function(resp){
+      success(resp)
+    })
+    .fail(function() {
+      console.error("Error fetching tweets: "  + resp)
+    })
 }
 
 /******************************
@@ -29,30 +31,6 @@ DV.api.twitter.stream = function(string, success, error){
 
 // Reference on XHR callbacks
 // http://stackoverflow.com/questions/5485495/how-can-i-take-advantage-of-callback-functions-for-asynchronous-xmlhttprequest
-
-// GET
-// takes target URL, callback, callback
-DV.api.get = function(url, success, error){
-	var xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function(){
-      
-      // If call is complete & successfull
-      if (xhr.readyState == 4 && xhr.status == 200) {
-        var resp = JSON.parse(xhr.response);
-        success(resp);
-
-      } else if (xhr.readyState == 4 && xhr.status != 200) {
-        console.log("API Call Error.");
-        console.log(xhr.response);
-
-        error(xhr);
-      }
-    };
-
-    xhr.open("GET", DV.url + url, false);
-    xhr.send();
-}
 
 DV.api.post = function(url, params, success, error){
 	var xhr = new XMLHttpRequest();
