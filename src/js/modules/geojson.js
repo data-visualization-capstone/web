48579f2114c3b1a1008517e34fec07bc8d4c4449
Alex Johnson <alex@tablelist.com>
2015-04-27 12:43:52 -0400

    Neighborhood styles

--- src/js/modules/geojson.js
+++ src/js/modules/geojson.js
@@ -4,7 +4,12 @@ function geoJsonLayer(json) {
   var layer = L.geoJson(json, {
     style: function(feature) {
       return {
-        color: feature.properties['COLOR']
+      	weight: 3,
+        opacity: 1,
+        color: '#666',
+        dashArray: '5',
+        fillOpacity: 0,
+        // color: feature.properties['COLOR'],
       }
     },
     onEachFeature: function (feature, layer) {
